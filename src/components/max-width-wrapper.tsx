673ef97efe3c682364ce61566f3aaa33f0857e1e
PachaiyappanV <pachaiyappan394@gmail.com>
2024-11-22 00:22:55 +0530

    feat(components): add `MaxWidthWrapper` component for layout styling
    - Created `MaxWidthWrapper` component to centralize max-width styling.
    - Utilizes the `cn` utility for dynamic class name merging.
    - Supports children rendering with optional custom class names.
    - Provides consistent max-width and padding for responsive layouts.

---
+++ src/components/max-width-wrapper.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/utils";
+
+interface MaxWidthWrapperProps {
+  children: React.ReactNode;
+  className?: string;
+}
+const MaxWidthWrapper = ({ children, className }: MaxWidthWrapperProps) => {
+  return (
+    <div
+      className={cn(
+        "h-full mx-auto max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +9,27 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        heading: ["var(--font-heading)", ...fontFamily.sans],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        brand: {
+          25: "#F7FAFD",
+          50: "#F0F5FA",
+          100: "#E1EBF5",
+          200: "#C7D7EB",
+          300: "#A9C3DE",
+          400: "#8BAECF",
+          500: "#6D99BF",
+          600: "#5483A3",
+          700: "#426A86",
+          800: "#335269",
+          900: "#273E4F",
+          950: "#1C2D3A",
+        },
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
